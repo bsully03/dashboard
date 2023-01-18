d316a2a6affd5be016f82a5f59f64ec526ef5734
Brian Sullivan <bcsullivan@guidehouse.com>
2023-01-18 08:05:41 -0500

    updates

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,17 +3,21 @@ import Card from './Card'
 
 export default function Main() {
   return (
-    <div class="Main">
-      <Card name = 'kpi1'/>
-      <Card name = 'kpi2'/>
-      <Card name = 'kpi3'/>
-      <Card name = 'kpi4'/>
-      <Card name = 'viz1'/>
-      <Card name = 'viz2'/>
-      <Card name = 'viz3'/>
-      <Card name = 'viz4'/>
-      <Card name = 'viz5'/>
-      <Card name = 'viz6'/>
+    <div class='main-contain'>
+      Hello
+      <div class="Main">
+        <Card name = 'kpi1'/>
+        <Card name = 'kpi2'/>
+        <Card name = 'kpi3'/>
+        <Card name = 'kpi4'/>
+        <Card name = 'viz1'/>
+        <Card name = 'viz2'/>
+        <Card name = 'viz3'/>
+        <Card name = 'viz4'/>
+        <Card name = 'viz5'/>
+        <Card name = 'viz6'/>
+      </div>
     </div>
+      
   )
 }
