77abd15ca31865156c4ade1dc25113b5aa7d6bf9
Brian Sullivan <bcsullivan@guidehouse.com>
2023-02-01 19:16:06 -0500

    getting the actual height and width

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import Chart from './chart'
+import React, { useState, useEffect, useRef } from "react";
+import Chart from "./chart";
 
 export default function Card(props) {
+  const [height, setHeight] = useState(0);
+  const [width, setwidth] = useState(0);
+  const elementRef = useRef(null);
+
+  const getSize = () => {
+    setHeight(elementRef.current.clientHeight * .95);
+    setwidth(elementRef.current.clientWidth * .95);
+  }
+
+  useEffect(() => {
+    getSize()
+  }, [elementRef]); //empty dependency array so it only runs once at render
+
+    useEffect(() => {
+      window.addEventListener("resize", getSize);
+    }, []);
 
   return (
-    <div className = 'Card'>
-      <Chart {...props} />  
+    <div ref={elementRef} className="Card" x={height}>
+      <Chart props = {props} height = {height} width={width}/>
     </div>
-  )
+  );
 }
