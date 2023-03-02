a44b5a67a684fd4bca7d977a146464839ae2c30e
Brian Sullivan <bcsullivan@guidehouse.com>
2023-03-02 16:55:50 -0500

    kpi line charts use real data

--- src/components/reChartsLine.jsx
+++ src/components/reChartsLine.jsx
@@ -10,64 +10,49 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import orders from "../orders.json";
 
 export default class ReChartsLine extends PureComponent {
   static demoUrl = "https://codesandbox.io/s/tiny-line-chart-r5z0f";
-
+  
+  
   render() {
+  
+  const metric = this.props.props.props.metric;
+  const data = orders.orders
+  
+  var arr = []
+  
+  function add(arr, ym, val) {
+    const found = arr.some((el) => el.date === ym);
+    if (!found) {
+      arr.push({ 'date': ym, 'val': val });
+    } else {
+      let obj = arr.find((f) => f.date === ym);
+      if (obj) obj.val += val 
+    }
+    return arr;
+  }
+  
+  for (var i = 0; i < data.length; i++) {
+    var date = new Date(data[i]['Order Date']);
+    var ym = date.getFullYear() + "-" + (date.getMonth()+1);
+    var val = data[i][metric]
+    arr = add(arr,ym, val)
+  }
+  
+  console.log(arr)
+    
     return (
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart width={300} height={100} data={data}>
+        <LineChart width={300} height={100} data={arr}>
           <Tooltip />
           <Line
             type="monotone"
-            dataKey="pv"
+            dataKey="val"
             stroke="#03647a"
             strokeWidth={2}
+            dot={false}
           />
         </LineChart>
       </ResponsiveContainer>
